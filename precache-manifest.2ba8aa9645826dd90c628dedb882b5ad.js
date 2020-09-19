ce2c6471f0f37f4695e5b8cc839b64129b6e4bf7
Mount Technologies <tigerofit@yahoo.com>
2020-09-20 00:19:47 +0500

    commit on 'master'

---
+++ precache-manifest.2ba8aa9645826dd90c628dedb882b5ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd1e1050d2ae214f84ce09282e350fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06028227c362b4b864c9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3132bec560dc80348a44",
+    "url": "/static/js/2.44fd95f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.44fd95f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06028227c362b4b864c9",
+    "url": "/static/js/main.73591a4d.chunk.js"
+  },
+  {
+    "revision": "b3cfd9f5cdf691e24e2f",
+    "url": "/static/js/runtime-main.00ed32f5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
